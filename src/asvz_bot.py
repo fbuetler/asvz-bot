a4beb382846731c3b386139a2abf558a3bcd9f76
Florian Buetler <flbuetle@ethz.ch>
2021-12-16 15:17:28 +0100

    python fmt

--- src/asvz_bot.py
+++ src/asvz_bot.py
@@ -141,10 +141,12 @@ class AsvzEnroller:
         weekday_int = time.strptime(WEEKDAYS[weekday], "%A").tm_wday
         weekday_date = today + timedelta((weekday_int - today.weekday()) % 7)
 
-        sport_url = f"{SPORTFAHRPLAN_BASE_URL}?"
-        +f"f[0]=sport:{sport_id}&"
-        +f"f[1]=facility:{FACILITIES[facility]}&"
-        +f"date={weekday_date.year}-{weekday_date.month:02d}-{weekday_date.day:02d}%20{start_time.hour:02d}:{start_time.minute:02d}"
+        sport_url = (
+            f"{SPORTFAHRPLAN_BASE_URL}?"
+            + f"f[0]=sport:{sport_id}&"
+            + f"f[1]=facility:{FACILITIES[facility]}&"
+            + f"date={weekday_date.year}-{weekday_date.month:02d}-{weekday_date.day:02d}%20{start_time.hour:02d}:{start_time.minute:02d}"
+        )
         logging.info("Searching lesson on '{}'".format(sport_url))
 
         lesson_url = None
